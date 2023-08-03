9e78cf58183f06bb6fe607f1bbeb3423397df994
Pratiksha Pandurang Gawande <107988088+pratugawande98@users.noreply.github.com>
2023-08-03 14:31:28 +0530

    Add files via upload
    CRUD

---
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@OpenAPIDefinition
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
